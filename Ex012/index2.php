ffbb6eabed3dd4ff9491eb1f06edaeeb98dee470
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-05-29 22:21:08 -0300

    .
    Ex013 contem while

--- Ex012/index2.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(){
-
-}
-$idade = $_POST["idade"];
-$altura = $_POST["altura"];
-echo "você tem $idade  e ". number_format($altura, 2, ",");
-?>
---
+++ Ex013/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ex013</title>
+</head>
+<body>
+<form method="POST">
+        <label>Digite um número</label>
+        <input type="number" name="numero"><br>
+        <button type="submit">tabuada</button>
+    </form>
+
+    <?php
+    $numero = $_POST["numero"];
+    $contador = 0;
+    while ($contador <= 10){
+        echo "$numero X $contador = " . $contador*$numero . "<br>";
+        $contador = $contador + 1; 
+    }
+?>
+</body>
+</html>
