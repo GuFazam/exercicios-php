e253c2ab34d88047a17be7d9e7936c13b7969ee5
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-05-30 12:00:31 -0300

    .

---
+++ Ex018/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ex018</title>
+</head>
+<body>
+    <form method="post">
+        <h3>Digite suas notas:</h3><br>
+        <label>Portugês</label>
+        <input type="number" name="nota1"><br>
+        <label>Matemática</label>
+        <input type="number" name="nota2"><br>
+        <label>Quimica</label>
+        <input type="number" name="nota3"><br>
+        <button type="submit">Calcular</button>
+    </form>
+    <?php
+    $nota1 = $_POST["nota1"];
+    $nota2 = $_POST["nota2"];
+    $nota3 = $_POST["nota3"];
+    $media = ($nota1 + $nota2 + $nota3) / 3;
+    echo "sua média é $media";
+    ?>
+</body>
+</html>
---
+++ Ex019/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ex019</title>
+</head>
+<body>
+    <form method="post">
+        <label>Digite um número</label>
+        <input type="number" name="numero"><br>
+        <button type="submit">Calcular</button>
+    </form>
+    <?php
+    $numero = $_POST["numero"];
+    if ($numero % 2 == 0){
+        echo "este numero é par";
+    } else {
+        echo "este numero é impar";
+    } 
+    ?>
+</body>
+</html>
