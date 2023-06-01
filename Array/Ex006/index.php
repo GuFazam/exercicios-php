aa591e1f2a7bd4fd214249a3b02e7926095ebe63
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-05-31 21:13:24 -0300

    array pronto
    exercicios de array terminados, falta apenas 2 de formulários

--- Array/Ex006/index.php
+++ Array/Ex006/index.php
@@ -1,3 +1,21 @@
 <?php
+$numeros = [10, 20, 30, 40, 50];
 
+$soma = 0;
+$Contador = 0;
+
+// Percorre os elementos do array e soma os valores
+foreach ($numeros as $numero) {
+    $soma += $numero;
+    $Contador++;
+}
+
+// Calcula a média
+if ($Contador > 0) {
+    $media = $soma / $Contador;
+    echo "A média dos números é: " . $media;
+} 
+else {
+    echo "O array está vazio.";
+}
 ?>
